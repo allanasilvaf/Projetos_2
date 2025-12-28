fc572418d64f3a922770495f6512068e9872d53c
ClaudioHNQ <contatohenrique97@gmail.com>
2025-12-28 13:46:53 -0300

    Update main.js
    update main para carregar o arquivo css global

--- gam-py/frontend/src/main.js
+++ gam-py/frontend/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'  // ← IMPORTANTE: importar o router
+import router from './router'
+import './assets/main.css'
 
 Vue.config.productionTip = false
 
