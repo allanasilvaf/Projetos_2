38556a88194e99bf8a1d635886fd48cee2b11808
ClaudioHNQ <contatohenrique97@gmail.com>
2025-12-30 17:08:42 -0300

    Update App.vue
    customização da barra de scroll

--- gam-py/frontend/src/App.vue
+++ gam-py/frontend/src/App.vue
@@ -33,3 +33,31 @@ body:has(#app > .app-header[style*="display: none"]) {
    padding-top: 0;
 }
 </style>
+
+<style>
+
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: #f4f6f8;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #a5c9a3; 
+  border-radius: 10px;       
+
+  border: 2px solid #f4f6f8; 
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #6BA368; 
+}
+
+html {
+  scrollbar-width: thin;          
+  scrollbar-color: #6BA368 #f4f6f8;
+}
+</style>
